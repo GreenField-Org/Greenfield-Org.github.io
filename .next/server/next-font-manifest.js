0694d3203227c4b6dfecdf4770f8dcfdbfe1bdba
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-07-11 23:17:42 -0400

    migrate site to NextJS

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/danielvigil/Development/Greenfield-Org.github.io/src/app/layout\":[\"static/media/2aaf0723e720e8b9-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
