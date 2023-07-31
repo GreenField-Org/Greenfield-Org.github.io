5559e04ad0f5600fd2363d9a699cb93be1395b49
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-07-30 21:22:01 -0400

    adding boilerplate for wave component

---
+++ src/components/Wave/index.tsx
@@ -0,0 +1,13 @@
+interface WaveProps {
+  color: string
+  direction: string
+}
+
+const Wave: React.FC<WaveProps> = ({
+  color, 
+  direction
+}) => {
+  return <>Test</>
+}
+
+export default Wave
