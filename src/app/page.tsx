fab401d618c687b15db8e2fadd9a2888d059c816
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-11-19 13:57:24 -0500

    homepage UX additions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,111 +2,109 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className='flex w-[375px] lg:w-[1440px] h-[900px] flex-col items-start lg:items-center gap-20 px-5 lg:px-16 py-0'>
+        <div className='flex flex-col justify-center items-start gap-6 flex-[1_0_0] self-stretch pr-0'>
+          <h1 className='self-stretch text-[color:var(--Black,#000)] text-[56px] not-italic font-bold leading-[120%]'>Empowering Innovation Through Inclusive Software Development</h1>
+          <p className='self-stretch text-[color:var(--Black,#000)] text-lg not-italic font-normal leading-[150%]'>At GreenField Dev, we are a LGBTQIA+ owned software development company dedicated to creating innovative solutions that drive positive change. With our expertise and commitment to social good, we work closely with our clients to meet their goals in a safe and respectful space.</p>
+          <div className='flex items-start gap-4 pt-4'>
+            <button className='flex justify-center items-center gap-2 border px-6 py-3 border-solid border-black'>Learn More</button>
+          </div>
+        </div>
+        <div></div>
+      </div>
+      <div className='flex w-[1440px] flex-col items-start gap-20 px-16 py-28'>
+        <div className='flex w-[1440px] flex-col items-start gap-20 px-16 py-28'>
+          <div className='flex items-start gap-20 self-stretch'>
+            <div className='flex flex-col items-start gap-4 flex-[1_0_0]'>
+              <p className='self-stretch text-[color:var(--Black,#000)] text-base not-italic font-semibold leading-[150%]'>Empowering</p>
+              <h2 className='self-stretch text-[color:var(--Black,#000)] text-5xl not-italic font-bold leading-[120%]'>Creating a Safe and Respectful Space for Clients</h2>
+            </div>
+            <div className='flex flex-col items-start gap-6 flex-[1_0_0]'>
+              <p className='self-stretch text-[color:var(--Black,#000)] text-lg not-italic font-normal leading-[150%]'>At GreenField Dev, we are committed to social good and strive to create a safe and respectful space for our clients. Through our LGBTQIA+ owned company, we prioritize inclusivity and work closely with our clients to help them achieve their goals.</p>
+              <div className='flex items-start gap-6 self-stretch px-0 py-2'>
+                <div className='flex flex-col items-start gap-4 flex-[1_0_0]'>
+                  <h6 className='self-stretch text-[color:var(--Black,#000)] text-xl not-italic font-bold leading-[140%]'>Inclusivity</h6>
+                  <p className='self-stretch text-[color:var(--Black,#000)] text-base not-italic font-normal leading-[150%]'>We embrace diversity and ensure that every client feels valued and respected.</p>
+                </div>
+                <div className='flex flex-col items-start gap-4 flex-[1_0_0]'>
+                  <h6 className='self-stretch text-[color:var(--Black,#000)] text-xl not-italic font-bold leading-[140%]'>Collabration</h6>
+                  <p className='self-stretch text-[color:var(--Black,#000)] text-base not-italic font-normal leading-[150%]'>We work hand in hand with our clients to understand their needs and deliver exceptional results.</p>
+                </div>
+                <div className='flex items-center gap-6 pt-4'>
+                  <button className='flex justify-center items-center gap-2 border px-6 py-3 border-solid border-black'>Learn More</button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='flex w-[1440px] flex-col items-start gap-20 px-16 py-28'>
+        <div className='flex items-center gap-20 self-stretch'>
+          <div className='flex flex-col items-start gap-10 flex-[1_0_0]'>
+            <div className='flex justify-center items-start gap-8 self-stretch'>
+              <h4 className='self-stretch text-[color:var(--Black,#000)] text-[32px] not-italic font-bold leading-[130%]'>Custom Software Development</h4>
+              <p className='self-stretch text-[color:var(--Black,#000)] text-base not-italic font-normal leading-[150%]'>We create tailored software solutions that meet your unique business needs. Our team of experienced developers will work closely with you to ensure a successful outcome.</p>
+            </div>
+            <div className='flex justify-center items-start gap-8 self-stretch'>
+              <h4 className='self-stretch text-[color:var(--Black,#000)] text-[32px] not-italic font-bold leading-[130%]'>Mobile App Development</h4>
+              <p className='self-stretch text-[color:var(--Black,#000)] text-base not-italic font-normal leading-[150%]'>We specialize in developing mobile applications for iOS and Android platforms. Our apps are user-friendly, visually appealing, and designed to enhance your brand's presence.</p>
+            </div>
+            <div className='flex justify-center items-start gap-8 self-stretch'>
+              <h4 className='self-stretch text-[color:var(--Black,#000)] text-[32px] not-italic font-bold leading-[130%]'>Web Development</h4>
+              <p className='self-stretch text-[color:var(--Black,#000)] text-base not-italic font-normal leading-[150%]'>Our web development services cover everything from creating responsive websites to building complex web applications. We focus on delivering high-quality solutions that drive results.</p>
+            </div>
+          </div>
+          <div className='h-[640px] flex-[1_0_0]'></div>
+        </div>
       </div>
+      <div className='flex w-[1440px] flex-col items-start gap-20 px-16 py-28'>
+        <div className='flex items-center gap-20 self-stretch'>
+          <div className='flex flex-col items-start gap-6 flex-[1_0_0]'>
+            <div className='flex flex-col items-start gap-8 self-stretch'>
+              <div className='flex flex-col items-start gap-4 self-stretch'>
+                <p className='text-[color:var(--Black,#000)] text-center text-base not-italic font-semibold leading-[150%]'>Innovation</p>
+                <h2 className='self-stretch text-[color:var(--Black,#000)] text-5xl not-italic font-bold leading-[120%]'>unlocking Potention through LGBTQIA+ Driven Software Solutions</h2>
+                <p className='self-stretch text-[color:var(--Black,#000)] text-lg not-italic font-normal leading-[150%]'>
+                At GreenField Dev, we are proud to be an LGBTQIA+ owned software development company. We believe in the power of diversity and inclusivity, and we leverage our unique perspective to deliver exceptional software solutions that meet your goals.
+                </p>
+              </div>
+            </div>
+            <div className='flex items-start gap-6 self-stretch px-0 py-2'>
+              <div className='flex flex-col items-start gap-2 flex-[1_0_0]'>
+                <h2 className='self-stretch text-[color:var(--Black,#000)] text-5xl not-italic font-bold leading-[120%]'>50%</h2>
+                <p className='self-stretch text-[color:var(--Black,#000)] text-base not-italic font-normal leading-[150%]'>Reduced Development Time with Agile Methodology</p>
+              </div>
+              <div className='flex flex-col items-start gap-2 flex-[1_0_0]'>
+                <h2 className='self-stretch text-[color:var(--Black,#000)] text-5xl not-italic font-bold leading-[120%]'>50%</h2>
+                <p className='self-stretch text-[color:var(--Black,#000)] text-base not-italic font-normal leading-[150%]'>Increased Customer Satisfaction with User-Centric Design</p>
+              </div>
+            </div>
+            <div className='flex items-center gap-6 pt-4'>
+              <button className='flex justify-center items-center gap-2 border px-6 py-3 border-solid border-black'>Learn More</button>
+            </div>
+          </div>
+          <div className='h-[640px] flex-[1_0_0]'></div>
+        </div>
+      </div>
+      <div className='flex w-[1440px] flex-col items-center gap-20 px-16 py-28'>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      </div>
+      <div className='flex w-[1440px] flex-col items-start gap-20 px-16 py-28'>
+        <div className='flex items-center gap-20 self-stretch'>
+          <div className='flex flex-col items-start gap-6 flex-[1_0_0]'>
+            <div className='flex w-[768px] flex-col items-start gap-6'>
+              <h3 className='self-stretch text-[color:var(--Black,#000)] text-[40px] not-italic font-bold leading-[120%]'>Transforming Ideas into Powerful Solutions</h3>
+              <p className='self-stretch text-[color:var(--Black,#000)] text-lg not-italic font-normal leading-[150%]'>
+                Contact us today for a personalized consultation of quote.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='flex items-start gap-4'>
+          <button className='flex justify-center items-center gap-2 border px-6 py-3 border-solid border-black'>Contact</button>
+          <button className='flex justify-center items-center gap-2 border px-6 py-3 border-solid border-black'>Get Started</button>
+        </div>
       </div>
     </main>
   )
