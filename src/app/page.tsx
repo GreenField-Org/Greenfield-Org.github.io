e3cb944f37558f59caef03c4f1375a363acbb3b9
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-11-19 23:27:40 -0500

    updating pages with new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
-import Image from 'next/image'
+import Button from '@/components/button/button'
+import Contact from '@/components/contact/contact'
+import Container from '@/components/container/container'
+import Cta from '@/components/cta/cta'
 
 export default function Home() {
   return (
     <main>
-      <div className='flex w-[1440px] h-[900px] flex-col items-center gap-20 px-16 py-0'>
+      <div className='flex w-[375px] lg:w-[1440px] h-[900px] flex-col items-center gap-20 px-5 lg:px-16 py-0'>
         <div className='flex items-center flex-[1_0_0] self-stretch'>
-          <div className='flex flex-col justify-center items-start gap-6 flex-[1_0_0] self-stretch pr-0'>
+          <div className='flex flex-col lg:justify-center items-start gap-6 lg:flex-[1_0_0] self-stretch px-0 py-16 lg:pr-0'>
             <div className='flex flex-col items-start gap-6 self-stretch'>
-              <h1 className='self-stretch text-[56px] not-italic font-bold leading-[120%]'>Empowering Innovation Through Inclusive Software Development</h1>
-              <p className='self-stretch text-lg not-italic font-normal leading-[150%]'>At GreenField Dev, we are a LGBTQIA+ owned software development company dedicated to creating innovative solutions that drive positive change. With our expertise and commitment to social good, we work closely with our clients to meet their goals in a safe and respectful space.</p>
+              <h1 className='self-stretch text-[40px] lg:text-[56px] not-italic font-bold leading-[120%]'>Empowering Innovation Through Inclusive Software Development</h1>
+              <p className='self-stretch text-base lg:text-lg not-italic font-normal leading-[150%]'>At GreenField Dev, we are a LGBTQIA+ owned software development company dedicated to creating innovative solutions that drive positive change. With our expertise and commitment to social good, we work closely with our clients to meet their goals in a safe and respectful space.</p>
             </div>
             <div className='flex items-start gap-4 pt-4'>
-              <button className='flex justify-center items-center gap-2 border px-6 py-3 border-solid border-black'>Learn More</button>
+              <Button buttonText='Learn More' />
             </div>
           </div>
-          <div className='flex h-[900px] items-start gap-4 flex-[1_0_0]'>
+          <div className='flex h-[460.305px] lg:h-[900px] items-start gap-[8.183px] lg:gap-4 flex-[1_0_0] w-[335px]'>
             <div className='flex w-[319.253px] h-[2732px] flex-col items-start gap-4 absolute bottom-[-1832px] right-0'></div>
             <div className='flex w-[319.253px] h-[2732px] flex-col items-start gap-4 absolute bottom-[-1680px]'></div>
           </div>
@@ -26,7 +29,7 @@ export default function Home() {
             <p className='self-stretch text-base not-italic font-semibold leading-[150%]'>Empowering</p>
             <h2 className='self-stretch text-5xl not-italic font-bold leading-[120%]'>Creating a Safe and Respectful Space for Clients</h2>
           </div>
-          <div className='flex flex-col items-start gap-6 flex-[1_0_0]'>
+          <div className='flex flex-col items-start gap-6 self-stretch flex-[1_0_0]'>
             <p className='self-stretch text-lg not-italic font-normal leading-[150%]'>At GreenField Dev, we are committed to social good and strive to create a safe and respectful space for our clients. Through our LGBTQIA+ owned company, we prioritize inclusivity and work closely with our clients to help them achieve their goals.</p>
             <div className='flex items-start gap-6 self-stretch px-0 py-2'>
               <div className='flex flex-col items-start gap-4 flex-[1_0_0]'>
@@ -39,12 +42,12 @@ export default function Home() {
               </div>
             </div>
             <div className='flex items-center gap-6 pt-4'>
-              <button className='flex justify-center items-center gap-2 border px-6 py-3 border-solid border-black'>Learn More</button>
+              <Button buttonText='Learn More' />
             </div>
           </div>
         </div>
       </div>
-      <div className='flex w-[1440px] flex-col items-start gap-20 px-16 py-28'>
+      <Container>
         <div className='flex items-center gap-20 self-stretch'>
           <div className='flex flex-col items-start gap-10 flex-[1_0_0]'>
             <div className='flex justify-center items-start gap-8 self-stretch'>
@@ -62,14 +65,14 @@ export default function Home() {
           </div>
           <div className='h-[640px] flex-[1_0_0]'></div>
         </div>
-      </div>
-      <div className='flex w-[1440px] flex-col items-start gap-20 px-16 py-28'>
+      </Container>
+      <Container>
         <div className='flex items-center gap-20 self-stretch'>
           <div className='flex flex-col items-start gap-6 flex-[1_0_0]'>
             <div className='flex flex-col items-start gap-8 self-stretch'>
               <div className='flex flex-col items-start gap-4 self-stretch'>
                 <p className='text-center text-base not-italic font-semibold leading-[150%]'>Innovation</p>
-                <h2 className='self-stretch  text-5xl not-italic font-bold leading-[120%]'>unlocking Potention through LGBTQIA+ Driven Software Solutions</h2>
+                <h2 className='self-stretch  text-5xl not-italic font-bold leading-[120%]'>Unlocking Potential through Innovative Software Solutions</h2>
                 <p className='self-stretch  text-lg not-italic font-normal leading-[150%]'>
                 At GreenField Dev, we are proud to be an LGBTQIA+ owned software development company. We believe in the power of diversity and inclusivity, and we leverage our unique perspective to deliver exceptional software solutions that meet your goals.
                 </p>
@@ -86,13 +89,13 @@ export default function Home() {
               </div>
             </div>
             <div className='flex items-center gap-6 pt-4'>
-              <button className='flex justify-center items-center gap-2 border px-6 py-3 border-solid border-black'>Learn More</button>
+              <Button buttonText='Learn More' />
             </div>
           </div>
           <div className='h-[640px] flex-[1_0_0]'></div>
         </div>
-      </div>
-      <div className='flex w-[1440px] flex-col items-center gap-20 px-16 py-28'>
+      </Container>
+      <Container>
         <div className='flex items-center gap-20 self-stretch'>
           <div className='h-[640px] flex-[1_0_0]'></div>
           <div className='flex flex-col items-start gap-8 flex-[1_0_0]'>
@@ -101,46 +104,9 @@ export default function Home() {
             <div className='flex items-center gap-5'></div>
           </div>
         </div>
-      </div>
-      <div className='flex w-[1440px] flex-col items-start gap-20 px-16 py-28'>
-        <div className='flex items-center gap-20 self-stretch'>
-          <div className='flex flex-col items-start gap-6 flex-[1_0_0]'>
-            <div className='flex w-[768px] flex-col items-start gap-6'>
-              <h3 className='self-stretch  text-[40px] not-italic font-bold leading-[120%]'>Transforming Ideas into Powerful Solutions</h3>
-              <p className='self-stretch  text-lg not-italic font-normal leading-[150%]'>
-                Contact us today for a personalized consultation of quote.
-              </p>
-            </div>
-          </div>
-          <div className='flex items-start gap-4'>
-          <button className='flex justify-center items-center gap-2 border px-6 py-3 border-solid border-black'>Contact</button>
-          <button className='flex justify-center items-center gap-2 border px-6 py-3 border-solid border-black'>Get Started</button>
-        </div>
-        </div>
-      </div>
-      <div className='flex w-[1440px] h-[1232px] flex-col items-center gap-20 px-16 py-28'>
-        <div className='flex items-start gap-20 self-stretch'>
-          <div className='flex flex-col items-start gap-4 flex-[1_0_0]'>
-            <p className='text-center text-base not-italic font-semibold leading-[150%]'>Innovative</p>
-            <div className='flex flex-col items-start gap-6 self-stretch'>
-              <h2 className='self-stretch text-5xl not-italic font-bold leading-[120%]'>Get in Touch</h2>
-              <p className='self-stretch text-lg not-italic font-normal leading-[150%]'>
-              We'd love to hear from you. Contact us for any inquiries or collaborations.
-              </p>
-            </div>
-          </div>
-          <div className='flex w-[500px] flex-col items-start gap-6 px-0 py-2'>
-            <div className='flex items-start gap-4 self-stretch'>
-              <div className='w-6 h6'></div>
-              <div className='flex flex-col items-start gap-2 flex-[1_0_0]'>
-                <h6 className='self-stretch text-xl not-italic font-bold leading-[140%]'>Email</h6>
-                <p className='self-stretch text-base not-italic font-normal leading-[150%] underline'>development_gf@gmail.com</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className='w-[1312px] h-[738px] shrink-0'></div>
-      </div>
+      </Container>
+      <Cta />
+      <Contact />
     </main>
   )
 }
