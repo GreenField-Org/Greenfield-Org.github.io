08a62432a38ea58ea4f7302e2eb30d596236961c
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-11-20 21:56:23 -0500

    updating container for mobile view

--- src/components/container/container.tsx
+++ src/components/container/container.tsx
@@ -6,7 +6,7 @@ export interface ContainerProps {
 
 export default function Container({children}: ContainerProps) {
     return (
-        <div className='flex w-[1440px] flex-col items-start gap-20 px-16 py-28'>
+        <div className='flex w-[375px] lg:w-[1440px] flex-col items-start gap-12 lg:gap-20 px-5 lg:px-16 py-16 lg:py-28'>
             {children}
         </div>
     )
